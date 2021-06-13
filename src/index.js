4ad1aa0f27b7b2e52ffec2228e1990191ff76aa7
XshubhamX <shubham.11901502@lpu.in>
2021-06-13 16:35:54 +0530

    serverchange

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://find-result.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
